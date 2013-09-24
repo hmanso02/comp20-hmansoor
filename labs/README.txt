953a2503212dcc384850d3129e6f7d62cc745411
Hamid <hamid@ubuntu.(none)>
2013-09-24 17:35:59 -0400

    Lab 1: Completed using a lot of help from http://wiki.apache.org/couchdb/Getting_started_with_Ruby

---
+++ labs/README.txt
@@ -0,0 +1,16 @@
+Hamid Mansoor
+Comp 120
+Lab 1
+
+It took me approximately 2 hours to complete this lab. 
+To the best of my knowledge, the lab has been completed correctly. 
+
+I took extensive help from the following link:
+
+http://wiki.apache.org/couchdb/Getting_started_with_Ruby
+
+The file mycouch.rb that I have pushed, has been written using 
+the sample code provided on the webpage above. I have only made
+small modifications to the code that I copied. The code that I found 
+on this website had very useful documentation on error handling 
+and basic commands such as making a database, document etc. 
---
+++ labs/mycouch.rb
@@ -0,0 +1,56 @@
+require 'net/http'
+ 
+module Couch
+ 
+   class Server
+     def initialize(host, port, options = nil)
+       @host = host
+       @port = port
+       @options = options
+     end
+
+    def delete(uri)
+       request(Net::HTTP::Delete.new(uri))
+     end
+ 
+     def get(uri)
+      request(Net::HTTP::Get.new(uri))
+    end
+ 
+     def put(uri, json)
+       req = Net::HTTP::Put.new(uri)
+       req["content-type"] = "application/json"
+       req.body = json
+      request(req)
+    end
+ 
+    def post(uri, json)
+       req = Net::HTTP::Post.new(uri)
+      req["content-type"] = "application/json"
+      req.body = json
+       request(req)
+    end
+
+    def request(req)
+       res = Net::HTTP.start(@host, @port) { |http|http.request(req) }
+       unless res.kind_of?(Net::HTTPSuccess)
+         handle_error(req, res)
+       end
+        res
+     end
+
+     private
+
+     def handle_error(req, res)
+      e = RuntimeError.new("#{res.code}:#{res.message}\nMETHOD:#{req.method}\nURI:#{req.path}\n#{res.body}")
+       raise e
+     end
+    end
+server = Couch::Server.new("67.23.79.113", "5984")
+server.put("/comp20-hmansoor/", "")
+doc = <<-JSON
+{"type":"comment","body":"First Post!"}
+JSON
+server.put("/comp20-hmansoor/document_id", doc)
+end
+
